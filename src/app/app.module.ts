7ba0d1c52347dbf1a425a371e0bd6c4583f9097a
Benjamin Garrido <benjamin.garrido@qrsof.com>
2022-07-22 15:22:22 -0500

    app routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TeachersComponent } from './teachers/teachers.component';
+import {RouterModule} from "@angular/router";
+import {AppRoutingModule} from "./app.routing.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeachersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,4 +1,17 @@
-import {Routes} from "@angular/router";
+import {RouterModule, Routes} from "@angular/router";
+import {TeachersComponent} from "./teachers/teachers.component";
+import {NgModule} from "@angular/core";
+
 const routes: Routes = [
-  {path: '', redirectTo: 'home'}
+  {path: '', redirectTo: 'teachers', pathMatch: 'full'},
+  {path: 'teachers', component: TeachersComponent},
 ]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {
+
+}
