9d6eb389b486fdc4d22e47a6246d99b813030296
Benjamin Garrido <benjamin.garrido@qrsof.com>
2022-07-23 00:29:51 -0500

    fix app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import {RouterModule, Routes} from "@angular/router";
 import {TeachersComponent} from "./teachers/teachers.component";
 import {NgModule} from "@angular/core";
+import {GroupsComponent} from "./teachers/groups/groups.component";
+import {ParentsComponent} from "./teachers/parents/parents.component";
 
 const routes: Routes = [
   {path: '', redirectTo: 'teachers', pathMatch: 'full'},
   {path: 'teachers', component: TeachersComponent },
+  {path: 'teachers/groups', component: GroupsComponent },
+  {path: 'teachers/parents', component: ParentsComponent },
 ]
 
 @NgModule({
