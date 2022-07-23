3bff4e51e4919522474e55e9267087f8a5a4df3c
Alexis Medina <alexis.medina@qrsof.com>
2022-07-22 19:03:35 -0500

    barra de navegacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,27 @@ import { AppComponent } from './app.component';
 import { TeachersComponent } from './teachers/teachers.component';
 import {RouterModule} from "@angular/router";
 import {AppRoutingModule} from "./app.routing.module";
+import {HomeComponent} from "./pages/portal/home/home.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatListModule} from "@angular/material/list";
+import {MatRadioModule} from "@angular/material/radio";
+import {NavBarComponent} from "./components/nav-bar/nav-bar.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    TeachersComponent
+    TeachersComponent,
+    HomeComponent,
+    NavBarComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    RouterModule
-  ],
+  exports: [MatSidenavModule],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        RouterModule,
+        BrowserAnimationsModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -3,7 +3,7 @@ import {TeachersComponent} from "./teachers/teachers.component";
 import {NgModule} from "@angular/core";
 
 const routes: Routes = [
-  {path: '', redirectTo: 'teachers', pathMatch: 'full'},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'teachers', component: TeachersComponent},
 ]
 
